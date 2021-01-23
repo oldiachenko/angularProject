95507260876f33d32db50bdabd67b8d7237ed5fc
ODiachenko <missredhair0@gmail.com>
2021-01-23 18:12:14 +0200

    lesson2

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {User} from '../../../models/User';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  @Input()
+  user!: User;
+  @Output()
+  bubbleUp = new EventEmitter <User> ();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  getUserInfo(user: User): void {
+this.bubbleUp.emit(user);
+  }
+
+}
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../../models/User';
+import {UserService} from '../../services/user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users!: User [];
+  singleUser!: User;
+
+  constructor(private userService: UserService) {
+  }
+
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(value => this.users = value);
+  }
+
+  getBubble(user: User): void {
+    this.singleUser = user;
+  }
+
+}
