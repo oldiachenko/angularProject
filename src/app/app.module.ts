f721c075b35491023556a637c247de9de7d8217e
ODiachenko <missredhair0@gmail.com>
2021-01-23 16:17:10 +0200

    clean branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
-import { UserAddressComponent } from './user-address/user-address.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    UsersComponent,
-    UserComponent,
-    UserAddressComponent
   ],
   imports: [
     BrowserModule,
